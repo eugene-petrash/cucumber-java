3d24842a2e9809f69a6e772d11801ba587564d6e
Eugene Petrash <petrash.evgeniy@gmail.com>
2018-01-22 22:20:43 +0200

    Working with multiple data usind Data Tables

--- src/test/java/Steps/LoginStep.java
+++ src/test/java/Steps/LoginStep.java
@@ -1,9 +1,11 @@
 package Steps;
 
-import cucumber.api.PendingException;
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+
+import java.util.List;
 
 /**
  * Created by petrash on 1/22/18.
@@ -17,6 +19,13 @@ public class LoginStep {
     @And("^I enter the username as \"([^\"]*)\" and password as \"([^\"]*)\"$")
     public void iEnterTheUsernameAsAndPasswordAs(String username, String password) throws Throwable {
         System.out.println("I enter the username as " + username + " and password as " + password);
+    }
+
+    @And("^I enter the following for Login$")
+    public void iEnterTheFollowingForLogin(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        System.out.println(data.get(0).get(0));
+        System.out.println(data.get(0).get(1));
     }
 
     @And("^I click login button$")
